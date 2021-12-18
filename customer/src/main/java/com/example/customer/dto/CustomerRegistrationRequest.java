697c83b6b2b1cff1435e61bf66a21e914d3df35d
nurahmet.azhibek <nurahmet.azhibek@btsdigital.kz>
2021-12-18 15:07:20 +0600

    maven settings, basic functionality of customer microservice

---
+++ customer/src/main/java/com/example/customer/dto/CustomerRegistrationRequest.java
@@ -0,0 +1,9 @@
+package com.example.customer.dto;
+
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+
+}
---
+++ customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.example.customer.service;
+
+import com.example.customer.dto.CustomerRegistrationRequest;
+import com.example.customer.model.Customer;
+
+public record CustomerService() {
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // todo check if email valid
+        // todo check if email not taken
+        // todo store customer in db
+    }
+}
